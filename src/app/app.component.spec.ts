d68505950947b5cfbcfca41476fca0146bbce595
Rowdy Rabouw <rowdy@double-r.nl>
2021-11-22 14:57:21 +0100

    feat: replace template with NFS content

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -5,12 +5,8 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -20,16 +16,9 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'nfs'`, () => {
+  it(`should have as title 'Need for Speed - Angular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('nfs');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('nfs app is running!');
+    expect(app.title).toEqual('Need for Speed - Angular');
   });
 });
---
+++ src/app/stills/stills.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-stills',
+  templateUrl: './stills.component.html',
+})
+export class StillsComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
